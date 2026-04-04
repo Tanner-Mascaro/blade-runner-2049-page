294a0755ce8834e0e4b1d1e83a58cd463f9c22fa
Tanner.Mascaro <tanner.mascaro9@gmail.com>
2026-04-04 15:31:46 -0600

    added new css page

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeTheme() {
+    let theme = document.getElementById("theme-style");
+
+    if (theme) {
+        theme.remove();
+    } else {
+        let link = document.createElement("link");
+        link.id = "theme-style";
+        link.rel = "stylesheet";
+        link.href = "css/theme.css";
+
+        document.head.appendChild(link);
+    }
+}
